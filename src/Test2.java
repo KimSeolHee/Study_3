7f51cd0385d24196cb12abd5e4cd555832a12e57
KimSeolHee <tjfgml64@nate.com>
2022-07-06 12:09:33 +0900

    test

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("테스트하기");
+		System.out.println("아무거나 올리기");
+	}
+
+}
